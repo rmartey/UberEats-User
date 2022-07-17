40b0a2b83a91943e84484c8cb75aaea94914b0a0
rmartey <marteyrichmond001@gmail.com>
2022-07-17 21:58:23 +0000

    added restaurant item

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import RestaurantItem from "./src/components/RestaurantItem";
+import restaurant from "./data/restaurants.json";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{/* Restaurant item */}
+			<RestaurantItem restaurant={restaurant[0]} />
+			<RestaurantItem restaurant={restaurant[1]} />
+
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 10,
+	},
 });
---
+++ src/components/RestaurantItem/index.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const RestaurantItem = ({ restaurant }) => {
+	//console.log(restaurant);
+	return (
+		<View style={styles.restaurantContainer}>
+			<Image
+				source={{
+					uri: restaurant.image,
+				}}
+				style={styles.image}
+			/>
+			<Text style={styles.title}>{restaurant.name}</Text>
+			<Text style={styles.subtitle}>
+				${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
+				{restaurant.maxDeliveryTime} minutes
+			</Text>
+		</View>
+	);
+};
+
+export default RestaurantItem;
+
+const styles = StyleSheet.create({
+	restaurantContainer: {
+		width: "100%",
+		marginVertical: 10,
+	},
+	image: {
+		width: "100%",
+		aspectRatio: 5 / 3,
+		marginBottom: 5,
+	},
+	title: {
+		fontSize: 18,
+		fontWeight: "bold",
+		marginVertical: 5,
+	},
+	subtitle: {
+		color: "grey",
+	},
+});
